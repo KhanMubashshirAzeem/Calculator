897ec4ea3744e89cf438aed0e8aefb127f39474c
Khan Mubashshir Azeem <khanmubashshir08@gmail.com>
2023-09-18 09:14:14 +0530

    Backend Completed

--- app/src/main/java/com/example/calculater/MainActivity.java
+++ app/src/main/java/com/example/calculater/MainActivity.java
@@ -13,7 +13,7 @@ import org.mozilla.javascript.Scriptable;
 
 import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView resultTv, solutionTv;
     MaterialButton buttonC, buttonBrackOpen, buttonBrackClose;
@@ -26,79 +26,79 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        resultTv = findViewById(R.id.result_tv);
-//        solutionTv = findViewById(R.id.solution_tv);
-//
-//        assignId(buttonC, R.id.button_c);
-//        assignId(buttonBrackOpen, R.id.button_open_bracket);
-//        assignId(buttonBrackClose, R.id.button_close_bracket);
-//        assignId(buttonDivide, R.id.button_divide);
-//        assignId(buttonMultiply, R.id.button_multiply);
-//        assignId(buttonPlus, R.id.button_plus);
-//        assignId(buttonMinus, R.id.button_minus);
-//        assignId(buttonEquals, R.id.button_equals);
-//        assignId(button0, R.id.button_0);
-//        assignId(button1, R.id.button_1);
-//        assignId(button2, R.id.button_2);
-//        assignId(button3, R.id.button_3);
-//        assignId(button4, R.id.button_4);
-//        assignId(button5, R.id.button_5);
-//        assignId(button6, R.id.button_6);
-//        assignId(button7, R.id.button_7);
-//        assignId(button8, R.id.button_8);
-//        assignId(button9, R.id.button_9);
-//        assignId(buttonAC, R.id.button_ac);
-//        assignId(buttonDot, R.id.button_dot);
-//    }
-//
-//    void assignId(MaterialButton btn, int id) {
-//        btn = findViewById(id);
-//        btn.setOnClickListener(this);
-//    }
-//
-//    @Override
-//    public void onClick(View view) {
-//        MaterialButton button = (MaterialButton) view;
-//        String buttonText = button.getText().toString();
-//        String dataToCalculate = solutionTv.getText().toString();
-//
-//        if (buttonText.equals("AC")) {
-//            solutionTv.setText("");
-//            resultTv.setText("0");
-//            return;
-//        }
-//        if (buttonText.equals("=")) {
-//            solutionTv.setText(resultTv.getText());
-//            return;
-//        }
-//        if (buttonText.equals("C")) {
-//            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
-//        } else {
-//            dataToCalculate = dataToCalculate + buttonText;
-//        }
-//        solutionTv.setText(dataToCalculate);
-//
-//        String finalResult = getResult(dataToCalculate);
-//
-//        if (!finalResult.equals("Err")) {
-//            resultTv.setText(finalResult);
-//        }
-//    }
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
 
-//    String getResult(String data) {
-//        try {
-//            Context context = Context.enter();
-//            context.setOptimizationLevel(-1);
-//            Scriptable scriptable = context.initStandardObjects();
-//            String finalResult = context.evaluateString(scriptable,data,"Javascript",1,null).toString();
-//            if (finalResult.endsWith(".0")){
-//                finalResult = finalResult.replace(".0","");
-//            }
-//            return finalResult;
-//        }catch (Exception E){
-//            return "Err";
-//        }
-//
+        assignId(buttonC, R.id.button_c);
+        assignId(buttonBrackOpen, R.id.button_open_bracket);
+        assignId(buttonBrackClose, R.id.button_close_bracket);
+        assignId(buttonDivide, R.id.button_divide);
+        assignId(buttonMultiply, R.id.button_multiply);
+        assignId(buttonPlus, R.id.button_plus);
+        assignId(buttonMinus, R.id.button_minus);
+        assignId(buttonEquals, R.id.button_equals);
+        assignId(button0, R.id.button_0);
+        assignId(button1, R.id.button_1);
+        assignId(button2, R.id.button_2);
+        assignId(button3, R.id.button_3);
+        assignId(button4, R.id.button_4);
+        assignId(button5, R.id.button_5);
+        assignId(button6, R.id.button_6);
+        assignId(button7, R.id.button_7);
+        assignId(button8, R.id.button_8);
+        assignId(button9, R.id.button_9);
+        assignId(buttonAC, R.id.button_ac);
+        assignId(buttonDot, R.id.button_dot);
+    }
+
+    void assignId(MaterialButton btn, int id) {
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTv.getText().toString();
+
+        if (buttonText.equals("AC")) {
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        }
+        if (buttonText.equals("=")) {
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
+        if (buttonText.equals("C")) {
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+        } else {
+            dataToCalculate = dataToCalculate + buttonText;
+        }
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if (!finalResult.equals("Err")) {
+            resultTv.setText(finalResult);
+        }
+    }
+
+    String getResult(String data) {
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult = context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            if (finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0","");
+            }
+            return finalResult;
+        }catch (Exception E){
+            return "Err";
+        }
+
     }
 
 }
